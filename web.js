086658f741d840ca52c583655122266e1341d44d
Pedro Vieira <pedro.vieira@mac.com>
2013-07-09 17:32:10 +0100

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var inFile = 'index.html'
+var inBuffer = new Buffer(255);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	inBuffer = fs.readFileSync(inFile);
+	response.send(inBuffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
