10a2c464068b150bcf479b86af0a532c4bd94633
psvieira <pedro.vieira@mac.com>
2013-09-06 10:59:44 +0100

    Updated heroku setup script

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
 echo -e "\n\nNOW ENTER YOUR HEROKU PASSWORD"
-# Set up heroku.
+# Set up heroku - staging and production evironments.
 # - devcenter.heroku.com/articles/config-vars
 # - devcenter.heroku.com/articles/heroku-postgresql
-heroku login
-# heroku create
+
+# This set of commands only needed if the environments do not exist already
+# heroku login
 # ssh-keygen -t rsa
 # heroku keys:add
-heroku addons:add heroku-postgresql:dev
-heroku plugins:install git://github.com/ddollar/heroku-config.git
+# heroku apps:create vegapi-s --remote heroku-staging
+# heroku addons:add heroku-postgresql:dev
+# heroku plugins:install git://github.com/ddollar/heroku-config.git
+# heroku pg:promote `heroku config  --app vegapi-s | grep HEROKU_POSTGRESQL | cut -f1 -d':'` --app vegapi-s
 
-# Setup heroku configuration variables from 2 sources:
-# - from .env-heroku for COINBASE_API_KEY and PORT
-# - from heroku pg:promote command for DATABASE_URL
+# heroku apps:create vegapi --remote heroku-production
+# heroku addons:add heroku-postgresql:dev
+# heroku plugins:install git://github.com/ddollar/heroku-config.git
+# heroku pg:promote `heroku config  --app vegapi | grep HEROKU_POSTGRESQL | cut -f1 -d':'` --app vegapi
+
+# Setup heroku configuration variables for COINBASE_API_KEY and PORT from .env-heroku
 # https://devcenter.heroku.com/articles/config-vars
 cp .env-heroku .env
-heroku pg:promote `heroku config  | grep HEROKU_POSTGRESQL | cut -f1 -d':'`
 
 STRING=$( cat <<EOF
 Now do the following:\n\n
 
 1) To deploy to heroku-staging\n
      $ git push heroku-staging\n
-     $ heroku config:push --app heroku-staging\n
+     $ heroku config:push --app vegapi-s\n
    Then check the corresponding Heroku URL\n\n
 2) To deploy to heroku-production\n
 	 $ git push heroku-production\n
-	 $ heroku config:push --app heroku-production\n
+	 $ heroku config:push --app vegapi\n
 EOF
 )
 echo -e $STRING
